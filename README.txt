e3085694b4fa7c87527881ba1703329a70b00341
Paweł Łucek <pawel@lucek.waw.pl>
2018-05-23 00:31:53 +0200

    README ADDED

---
+++ README.txt
@@ -0,0 +1,4 @@
+This project is for learning purposes only
+
+Day 1:
+Displaying video on the screen in windows form control
---
+++ WebCamTracking/WebCam.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+
+namespace WebCamTracking
+{
+    class WebCam:IDisposable
+    {
+        private readonly VideoCapture capture;
+        public WebCam()
+        {
+            capture = new VideoCapture();
+        }
+        public Bitmap CurrentFrame()
+        {
+            capture?.Start(); //Starting capture just for capturing the frame
+            Bitmap frame = capture.QueryFrame().Bitmap;
+            capture?.Stop(); //Stoping capture to prevent memory leak
+            return frame;            
+        }
+
+        public void Dispose()
+        {
+            capture?.Dispose();
+            capture?.Stop();
+        }
+    }
+}
